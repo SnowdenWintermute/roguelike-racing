c11393302f6af323f5ff88209d6a0ae117edc5fd
Mike Silverman <michael.p.silverman@gmail.com>
2024-01-25 15:45:14 -0500

    added welcome message

--- client/src/components/lobby/mod.rs
+++ client/src/components/lobby/mod.rs
@@ -3,9 +3,11 @@ pub mod game_list_item;
 pub mod game_setup;
 pub mod lobby_menu;
 pub mod user_list;
+mod welcome_info;
 use crate::components::lobby::game_list::GameList;
 use crate::components::lobby::lobby_menu::LobbyMenu;
 use crate::components::lobby::user_list::UserList;
+use crate::components::lobby::welcome_info::WelcomeInfo;
 use yew::prelude::*;
 
 #[function_component(Lobby)]
@@ -13,8 +15,11 @@ pub fn lobby() -> Html {
     html!(
         <main class="min-h-screen w-screen p-4 bg-slate-800 text-zinc-300 flex flex-col">
             <LobbyMenu/>
-            <div class="w-full flex flex-1">
-                <GameList/>
+            <div class="w-full flex flex-1" >
+                <div class="w-full flex flex-col" >
+                    <WelcomeInfo />
+                    <GameList/>
+                </div>
                 <UserList/>
             </div>
         </main>
---
+++ client/src/components/lobby/welcome_info.rs
@@ -0,0 +1,26 @@
+use yew::prelude::*;
+
+#[function_component(WelcomeInfo)]
+pub fn welcome_info() -> Html {
+    html!(
+        <section class="flex-1 p-4 mb-4 mr-4 bg-slate-700 border border-slate-400 overflow-y-scroll">
+            <h3 class="text-lg">{"Roguelike Racing"}</h3>
+            <h5 class="mb-2">
+            {"alpha 0.2.0"}
+            </h5>
+            <p class="mb-2">
+            {"Welcome to the early alpha of Roguelike Racing, a multiplayer turn based
+            RPG in the spirit of For the King, Diablo and Final Fantasy. All layout and graphics are placeholders.
+            There is a minimum playable game in which you can do the following:"}
+            </p>
+            <ul class="list-disc list-inside" >
+                <li>{"Create and join games which consist of one or more adventuring parties"}</li>
+                <li>{"Create a party with one or more players each controlling one or more characters"}</li>
+                <li>{"Explore rooms of the dungeon which may contain monsters"}</li>
+                <li>{"Battle monsters and receive randomly generated equipment"}</li>
+                <li>{"Equip, trade and discard equipment"}</li>
+                <li>{"Try to reach the lowest floor of the dungeon by descending the stairs"}</li>
+            </ul>
+        </section>
+    )
+}
